6466ba90034ca9a7f6fa21da144e7f31bc606944
ArnelS72 <arnel.seferovic@hotmail.com>
2015-12-08 11:30:25 +0100

    Lab1

---
+++ Lab1/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
+//uppgift 1
+//using System;
+
+//namespace Simple
+//{
+//    class Program   //declare a class
+//    {
+//        static void Main()
+//        {
+//            Console.WriteLine("Hi there!");
+//            Console.ReadLine();
+//        }
+//    }
+//}
+
+
+////Upg 2:
+////using System;
+//namespace Simple //uppgift 2
+//{
+//    klass Program
+//    {
+//        static void Maijn()
+//{
+//            Console.WriteLine("Hi there!")
+//    Console.ReadLine();
+//        }
+//    }
+//}
+
+//// koder med fel
+
+
+
+    /////////////////////////////////upg 3
+//    using System;
+//namespace Simple //uppgift 3
+//{
+//    class Program
+//    {
+//        static void Main()
+//        {
+//            //System.Console.WriteLine("Hi there!");
+//            //System.Console.WriteLine("Second row");
+//            //System.Console.WriteLine("third row");
+//            ////Console.WriteLine kan skriva text i flera rad.
+
+//            //System.Console.Write("Hello,second,third row");
+//            ////Console.Write skriver text i en rad
+
+//            Console.WriteLine("Three interges are {1},{0} and {1}.", 3, 6);
+//            //Format string
+//            Console.ReadLine();
+//        }
+//    }
+//}
+
+    /////////////////////////Upg 4
+//    using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Uppgift4
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            /*1 i bin Debug försvann :
+//                Uppgift1.pdb
+//                Uppgift1.vhost.exe
+
+//                obj Debug försvann:
+//                Uppgif1.csproj.FileListAbsolut.txt
+//                Uppgift.sprojResolveAssembly
+//                Uppgift1.exe
+//                Uppgift1.pdb
+//           */
+
+//        }
+//    }
+//}
